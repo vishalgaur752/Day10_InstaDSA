98190909ad773f2287d584738bfce7efb79766fe
vishalgaur752 <vishalgaur752@gmail.com>
2024-05-28 19:48:02 +0530

    InstaDSA

---
+++ MaximumNestingDepth.java
@@ -0,0 +1,20 @@
+public class MaximumNestingDepth {
+    public static int maxnestDepth(String s) {
+        int max = 0;
+        int curr = 0;
+        for(int i = 0; i<s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                curr++;
+            }
+            if(s.charAt(i) == ')') {
+                curr--;
+            }
+            max = Math.max(max, curr);
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        String s = "(1+(2+3) + ((8)/4)) + 1";
+        System.out.println(maxnestDepth(s));
+    }
+}
---
+++ SumOfBueaty.java
@@ -0,0 +1,5 @@
+public class SumOfBueaty {
+    public static void main(String[] args) {
+        
+    }    
+}
