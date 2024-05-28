67eb053503c216a6bc5ce0292733a8d81fec47db
vishalgaur752 <vishalgaur752@gmail.com>
2024-05-28 20:59:08 +0530

    InstaDSA

--- LongestPalindromicSubStr.java
+++ LongestPalindromicSubStr.java
@@ -11,23 +11,24 @@ public class LongestPalindromicSubStr {
         }
         return true;
     }
+
     public static String longestPalindrome(String s) {
         int max = 0;
         int start = 0;
         int end = 0;
         int n = s.length();
-        for(int i = 0; i<n; i++) {
-            for(int j = i; j<n; j++) {
-                if(isPalindrome(s, i, j) == true) {
-                    if((j - i + 1) > max) {
-                        max = j - i +1;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (isPalindrome(s, i, j) == true) {
+                    if ((j - i + 1) > max) {
+                        max = j - i + 1;
                         start = i;
                         end = j;
                     }
                 }
             }
         }
-        return s.substring(start, end+1);
+        return s.substring(start, end + 1);
     }
 
     public static void main(String[] args) {
